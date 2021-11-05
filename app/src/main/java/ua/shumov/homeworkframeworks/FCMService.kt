eb86a997b7057cf2f78e78bd8c20ec7a31f1992d
Noshum <m.o.shumov@gmail.com>
2021-11-06 00:01:27 +0200

    FirebaseCloudMessaging

---
+++ app/src/main/java/ua/shumov/homeworkframeworks/FCMService.kt
@@ -0,0 +1,46 @@
+package ua.shumov.homeworkframeworks
+
+import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+class FCMService : FirebaseMessagingService() {
+
+    val TAG = "Service"
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        Log.d(TAG, "From: " + remoteMessage.from)
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.notification!!.body!!)
+        sendNotification(remoteMessage)
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.putExtra("message", remoteMessage.notification?.body!!)
+        startActivity(intent)
+    }
+
+    @SuppressLint("UnspecifiedImmutableFlag")
+    private fun sendNotification(remoteMessage: RemoteMessage) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_ONE_SHOT)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this)
+            .setContentText(remoteMessage.notification!!.body)
+            .setAutoCancel(true)
+            .setSmallIcon(R.drawable.ic_stat_pimp)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+    }
+}
